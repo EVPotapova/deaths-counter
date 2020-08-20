bbb3f2b16f70c37e176360737b4c3ff92105304e
Ekaterina Potapova <ekaterina.potapova@sharp-dev.net>
2020-08-20 15:02:24 +0300

    Initial commit

---
+++ scripts/counter.js
@@ -0,0 +1,34 @@
+function saveToFirebase(nickName) {
+
+    var value = document.getElementById(nickName).value;
+
+  var counterObject = {
+    nickName: nickName,
+    number: number,
+  };
+
+  firebase
+    .database()
+    .ref("deaths-count")
+    .push()
+    .set(counterObject)
+    .then(
+      function (snapshot) {
+        alert("Changed Succesfully!");
+      },
+      function (error) {
+        alert("Oops, something goes wrong. Call Samara IMMEDIATELY!!!");
+      }
+    );
+
+    getAllFromFirebase();
+}
+
+
+function getAllFromFirebase() {
+    var dbRef = firebase.database().ref("deaths-count");
+  
+    dbRef.once('value').then(function(snap) {
+        console.log(snap.val());    
+    });
+  }
